ff59f126aa67b5fb4163f111db4265a4ed4cef6a
anandhurm <anandhurm8056@gmail.com>
2026-06-17 20:57:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,55 @@ function updateDateTime() {
 
 updateDateTime();
 setInterval(updateDateTime, 1000);
+// Firebase Imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getFirestore,
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyAPRw8rEEzOMXwgDFdRNs9OSJEFH9znfy8",
+  authDomain: "dheiva-theervu.firebaseapp.com",
+  projectId: "dheiva-theervu",
+  storageBucket: "dheiva-theervu.firebasestorage.app",
+  messagingSenderId: "527566367493",
+  appId: "1:527566367493:web:0bcee3d5806516c3b5656c",
+  measurementId: "G-FM8NZVJKBR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Load Panchangam Data
+async function loadPanchangam() {
+
+    const docRef = doc(db, "daily_panchangam", "today");
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+
+        const data = docSnap.data();
+
+        document.getElementById("nakshatra").innerText = data.nakshatra;
+        document.getElementById("tithi").innerText = data.tithi;
+        document.getElementById("rahu").innerText = data.rahu;
+        document.getElementById("yamagandam").innerText = data.yamagandam;
+        document.getElementById("kuligai").innerText = data.kuligai;
+        document.getElementById("sunrise").innerText =
+            data.sunrise + " | " + data.sunset;
+        document.getElementById("festival").innerText = data.festival;
+        document.getElementById("rasi").innerText = data.rasi;
+
+    } else {
+
+        console.log("No Panchangam Data Found");
+
+    }
+}
+
+loadPanchangam();
